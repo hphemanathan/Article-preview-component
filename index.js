e17f0d455f7fbe8c29f3b361def15fa3a7ef641e
Hemanathan H P <hphemanathan@gmail.com>
2024-03-08 18:05:14 +0530

    wip

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const shareButton = document.querySelector(".section__author__share--icon");
+const shareComponent = document.querySelector(".section__share");
 
-console.log (shareButton);
+console.log (shareButton, shareComponent);
+
+function shareHandle(event) {
+    shareComponent.classList.add("open")
+}
+
+shareButton.addEventListener('click', shareHandle)
